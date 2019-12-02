d675e08354f99bfacea805a15401197503910c8d
MACmidiDev <mcamacho1990@icloud.com>
2019-12-02 11:38:51 +0000

    debugS3cssIcons

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,8 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
-
 if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 else:
@@ -96,7 +94,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 #  Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
